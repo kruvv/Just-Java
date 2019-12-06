9adf6bdd69bae232c0eb13b172fbaae0ebb4df6f
viktor <kruvv@mail.ru>
2019-12-06 18:07:53 +0300

    Add intent for send order

--- app/src/main/java/com/kruvv/justjava/MainActivity.java
+++ app/src/main/java/com/kruvv/justjava/MainActivity.java
@@ -2,12 +2,15 @@ package com.kruvv.justjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -18,9 +21,12 @@ import java.text.NumberFormat;
 public class MainActivity extends AppCompatActivity {
 
     int quantity = 0;
+    int whippedCream = 1;
+    int chocolate = 2;
     EditText editName;
     boolean hasWhippedCream;
     boolean hasChocolate;
+    String priceMessage;
 
 
     @Override
@@ -35,13 +41,23 @@ public class MainActivity extends AppCompatActivity {
     public void submitOrder(View view) {
         editName = findViewById(R.id.edit_name_edit_text);
 
-        String priceMessage = "Name: " + editName.getText() +
-                "\nAdd whipped creame? " + hasWhippedCream +
-                "\nAdd chocolate? " + hasChocolate +
-                 "\nQuantity: " + quantity +
-                    "\nTotal $" + calculatePrice(quantity) +
-                        "\nThank you!";
-        displayMessage(priceMessage);
+        priceMessage = getString(R.string.name) + editName.getText();
+        priceMessage += "\n" + getString(R.string.add_whipped_cream) + hasWhippedCream;
+        priceMessage += "\n" + getString(R.string.add_chocolate) + hasChocolate;
+        priceMessage += "\n" + getString(R.string.quantity_two) + quantity;
+        priceMessage += "\n" + getString(R.string.total) + createOrderSummary(hasWhippedCream, hasChocolate);
+        priceMessage += "\n" + getString(R.string.thank_you);
+
+        //displayMessage(priceMessage);
+
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     /**
@@ -57,10 +73,16 @@ public class MainActivity extends AppCompatActivity {
      * This method increments the value of the variable on the screen.
      */
     public void increment(View view) {
-        if(quantity >= 0){
+        if(quantity >= 0 & quantity < 100){
             quantity++;
             displayQuantity(quantity);
+            int summary = createOrderSummary(hasWhippedCream, hasChocolate);
+            displayMessage(summary);
+        }else if (quantity == 100) {
+            Toast toast = Toast.makeText(getApplicationContext(), R.string.more_then, Toast.LENGTH_SHORT);
+            toast.show();
         }
+
 
     }
 
@@ -68,19 +90,24 @@ public class MainActivity extends AppCompatActivity {
      * This method decrements the value of the variable on the screen.
      */
     public void decrement(View view) {
-        if(quantity >= 2){
+        if(quantity >= 2) {
             --quantity;
             displayQuantity(quantity);
+            int summary = createOrderSummary(hasWhippedCream, hasChocolate);
+            displayMessage(summary);
+        }else if (quantity == 1){
+            Toast toast = Toast.makeText(getApplicationContext(), R.string.less_then, Toast.LENGTH_SHORT);
+            toast.show();
         }
-
     }
 
     /**
      * This method displays the given text on the screen.
      */
-    private void displayMessage(String message) {
+    private void displayMessage(int summary) {
         TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
+        orderSummaryTextView.setText("$" + summary);
+
     }
 
     /**
@@ -110,7 +137,20 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method crate order
      */
-    public void createOrderSummary(boolean hasWhippedCream, boolean hasChocolate) {
+    public int createOrderSummary(boolean hasWhippedCream, boolean hasChocolate) {
+
+        int sum = 0;
+
+        if (hasWhippedCream & !hasChocolate ) {
+             sum = calculatePrice(quantity) + whippedCream * quantity;
+        }else if (!hasWhippedCream & hasChocolate) {
+            sum = calculatePrice(quantity) + chocolate * quantity;
+        }else if (hasWhippedCream & hasChocolate) {
+            sum = calculatePrice(quantity) + (chocolate + whippedCream) * quantity;
+        }else {
+            sum = calculatePrice(quantity);
+        }
+        return sum;
 
     }
 }
